a3483f3e01c4e9ee8178a2433b0f42891df6bf3f
gxyrafis <g.xyrafis@outlook.com.gr>
2024-04-18 02:55:01 +0300

    Added Alpha-Beta pruning player Similar to the minmax player, but considerably faster since the alpha-beta pruning player does not need to go through the entire tree. Also removed some redundant lines of code.

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ from tictactoeclass import TicTacToe
 from manualplayer import manual_player
 from minmaxplayer import minmax_player
 from randomplayer import random_player
+from a_b_player import a_b_player
 
 while True:
     game = TicTacToe()
     gametype = input("Press \"1\" to play against the MinMax Algorithm.\nPress \"2\" to have the random player play against it.\n"
+                     "Press \"3\" to play against the A-B Algorithm.\nPress \"4\" to have the random player play against it.\n"
                      "Press \"q\" to exit\n>$")
     if gametype == "1":
         while True:
             order = input("Who goes first? Press \"I\" for yourself or \"M\" for the MinMax Algorithm\n>$")
-            if order == "I":
+            if order.upper() == "I":
                 utility = game.play_game(manual_player, minmax_player)
                 break
-            elif order == "M":
+            elif order.upper() == "M":
                 utility = game.play_game(minmax_player, manual_player)
                 break
             else:
@@ -27,15 +29,37 @@ while True:
     elif gametype == "2":
         while True:
             order = input("Who goes first? Press \"R\" for the random player or \"M\" for the MinMax Algorithm\n>$")
-            if order == "R":
+            if order.upper() == "R":
                 utility = game.play_game(random_player, minmax_player)
                 break
-            elif order == "M":
+            elif order.upper() == "M":
                 utility = game.play_game(minmax_player, random_player)
                 break
             else:
                 print("Invalid input!\n")
-    elif gametype == "q":
+    elif gametype == "3":
+        while True:
+            order = input("Who goes first? Press \"I\" for yourself or \"AB\" for the A-B Algorithm\n>$")
+            if order.upper() == "I":
+                utility = game.play_game(manual_player, a_b_player)
+                break
+            elif order.upper() == "AB":
+                utility = game.play_game(a_b_player, manual_player)
+                break
+            else:
+                print("Invalid input!\n")
+    elif gametype == "4":
+        while True:
+            order = input("Who goes first? Press \"R\" for the random player or \"AB\" for the MinMax Algorithm\n>$")
+            if order.upper() == "R":
+                utility = game.play_game(random_player, a_b_player)
+                break
+            elif order.upper() == "AB":
+                utility = game.play_game(a_b_player, random_player)
+                break
+            else:
+                print("Invalid input!\n")
+    elif gametype.lower() == "q":
         exit(0)
     else:
         print("Invalid input!\n")
--- randomplayer.py
+++ randomplayer.py
@@ -3,7 +3,6 @@ from numpy import random
 def random_player(game, state):
     #AI player that chooses one of the available actions at random
     #with equal chances of each action being chosen
-    player = game.to_move(state)
     actions = game.actions(state)
 
     while True:
